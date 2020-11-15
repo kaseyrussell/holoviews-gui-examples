5796cd372482f5712c405318d2374819b5c5dc6c
kaseyrussell <kasey.russell@gmail.com>
2020-11-15 06:31:00 -0500

    Added info to readme and added csv save

--- README.md
+++ README.md
@@ -1,8 +1,36 @@
 # holoviews-gui-examples
 
-Noodling around with Holoviews and Bokeh for scientific instrument control interfaces. Easier than LabView?
+Are you doing hardware automation? Let's say you need to quickly read out some temperature data from a 
+[LabJack](https://labjack.com) or control a stepper motor. Building libraries to communicate with instruments 
+is often trivial in Python, but making a GUI interface (especially with a plot) can be cumbersome using
+standard GUI toolkits like Tk, Qt, or wxPython. 
 
-Here are a couple examples:
-1. Single timeseries data acquisition [timeseries_plot_single_variable.py](timeseries_plot_single_variable.py)
+The data science field has recently been building some amazing tools for interacting with data.
+Compared to the standard GUI toolkits, these data science GUI toolkits are much more along the lines of what 
+I typically need as a scientist and engineer.
+
+This repo has a few example GUIs built using [Holoviews](http://holoviews.org) and 
+[Bokeh](https://bokeh.org). These aren't drastically different than the examples in the guide to 
+[Working with Streaming Data](http://holoviews.org/user_guide/Streaming_Data.html) in the Holoviews documentation,
+but if you're not very familiar with Holoviews and DynamicMaps in particular, there are some meaningful
+differences. Plotting multiple lines took me much longer than it should have, for example.
+
+There's no actual hardware interaction here, but you could easily swap out the [fake_instrument.py](fake_instrument.py)
+module and replace it with a module for communicating with a real piece of hardware. 
+
+## Examples
+1. [timeseries_plot_single_variable.py](timeseries_plot_single_variable.py)
 ![timeseries_plot_single_variable.py](screenshots/timeseries_plot_single_variable.gif)
+
+## Setup
+I'm assuming you're using Anaconda Python.
+
+1. Clone this repo onto your computer
+2. Open an Anaconda prompt and navigate to the folder that contains this README.md file.
+3. To create a new environment with all the necessary packages, type this command at the Anaconda prompt:
+`> conda env create -f environment.yml`
+4. Activate the environment you just created by typing this command at the prompt:
+`> conda activate holoviews-gui-examples'
+5. Try out one of the examples. Running this command should open a new browser tab with the GUI in it:
+`> bokeh serve --show timeseries_plot_single_variable.py`
 
--- timeseries_plot_single_variable.py
+++ timeseries_plot_single_variable.py
@@ -1,5 +1,5 @@
 # Run with 
-# bokeh serve --show <name>
+# bokeh serve --show timeseries_plot_single_variable.py
 import pandas as pd
 import panel as pn
 import holoviews as hv
@@ -16,7 +16,7 @@ instrument = FakeInstrument()
 
 #
 # Make a buffer to hold the instrument data. You could get by without making a Pandas Dataframe, but it does a good
-# job of handling things like writing to a csv file.
+# job of writing to a csv file.
 #
 def make_df(time_sec=0.0, temperature_degC=0.0):
     return pd.DataFrame({'Time (s)': time_sec, 'Temperature (°C)': temperature_degC}, index=[0])
@@ -24,20 +24,41 @@ def make_df(time_sec=0.0, temperature_degC=0.0):
 #
 # Initialize the plot. Holoviews handles all of the plotting and makes some guesses about what columns to plot.
 #
-example_df = make_df()
+example_df = pd.DataFrame(columns=make_df().columns)
 buffer = Buffer(example_df, length=1000, index=False)
 plot = hv.DynamicMap(hv.Curve, streams=[buffer]).opts(padding=0.1, width=600, xlim=(0, None))
 
 #
 # Define any other GUI components
 #
-button = Button(label="Start")
+LABEL_START = 'Start'
+LABEL_STOP = 'Stop'
+button_startstop = Button(label=LABEL_START)
+LABEL_CSV_START = "Save to csv"
+LABEL_CSV_STOP = "Stop save"
+CSV_FILENAME = 'tmp.csv'
+
+button_csv = Button(label=LABEL_CSV_START)
 offset = Slider(title='Offset', start=-10.0, end=10.0, value=0.0, step=0.1)
 interval = Spinner(title="Interval (sec)", value=0.1, step=0.01)
 
-acquisition_task = None
 
+#
+# Define behavior
+#
+acquisition_task = None
+save_to_csv = False
+
+
+#
+# Here we're using a coroutine to handle getting the data from the instrument and plotting it without blocking
+# the GUI. In my experience, this works fine if you are only trying to get data from your instrument once every
+# ~50 ms or so. The buffering and plotting take on the order of 10-20 ms. If you need to communicate with your
+# instrument on millisecond timescales, then you'll want a separate thread and maybe even separate hardware. And you
+# won't want to update the plot with every single data point.
+#
 async def acquire_data(interval_sec=0.1):
+    global save_to_csv
     t0 = time.time()
     while True:
         instrument.set_offset(offset.value)
@@ -45,26 +66,49 @@ async def acquire_data(interval_sec=0.1):
         value = instrument.read_data()
         b = make_df(time_elapsed, value)
         buffer.send(b)
+
+        if save_to_csv:
+            b.to_csv(CSV_FILENAME, header=False, index=False, mode='a')
+
         time_spent_buffering = time.time() - t0 - time_elapsed
         if interval_sec > time_spent_buffering:
             await asyncio.sleep(interval_sec - time_spent_buffering)
 
+
+def toggle_csv():
+    global save_to_csv
+    if button_csv.label == LABEL_CSV_START:
+        button_csv.label = LABEL_CSV_STOP
+        example_df.to_csv(CSV_FILENAME, index=False)  # example_df is empty, so this just writes the header
+        save_to_csv = True
+    else:
+        save_to_csv = False
+        button_csv.label = LABEL_CSV_START
+
+
 def start_stop():
-    global acquisition_task
-    if button.label == 'Start':
-        button.label = 'Stop'
+    global acquisition_task, save_to_csv
+    if button_startstop.label == LABEL_START:
+        button_startstop.label = LABEL_STOP
         buffer.clear()
         acquisition_task = asyncio.get_running_loop().create_task(acquire_data(interval_sec=interval.value))
     else:
-        button.label = 'Start'
         acquisition_task.cancel()
+        button_startstop.label = LABEL_START
+        if save_to_csv:
+            toggle_csv()
 
-button.on_click(start_stop)
+
+button_startstop.on_click(start_stop)
+button_csv.on_click(toggle_csv)
+
 hv.extension('bokeh')
 hv.renderer('bokeh').theme = 'caliber'
 controls = pn.WidgetBox('# Controls',
                         interval,
-                        button,
+                        button_startstop,
+                        button_csv,
                         offset)
+
 pn.Row(plot, controls).servable()
 
